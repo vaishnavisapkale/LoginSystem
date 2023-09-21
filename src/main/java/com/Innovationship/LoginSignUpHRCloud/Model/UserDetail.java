2d57ff07aca4b27b345ea0d4ddff334f11612060
VaishnaviSapkale <vaishusapkale17@gmail.com>
2023-09-21 17:48:25 +0530

    login system

---
+++ src/main/java/com/Innovationship/LoginSignUpHRCloud/Model/UserDetail.java
@@ -0,0 +1,17 @@
+package com.Innovationship.LoginSignUpHRCloud.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class UserDetail {
+      
+    @Id
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+
+}
